046a92951830b6c48c016fc2248aad0687bfae9f
hamza887 <hamzabutt887>
2021-07-14 08:28:45 -0400

    adding navbar completed

--- myportfolio-app/src/components/Navbar.js
+++ myportfolio-app/src/components/Navbar.js
@@ -3,23 +3,29 @@ import logo from '../Assets/HZ.png'
 export default function Nav(){
     return(
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a className="navbar-brand" href="#">HZ</a>
+          <div className='container'>
+        <a className="navbar-brand logo" href="#">HZ</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
+
         </button>
       
         <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-          <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+          <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
             <li className="nav-item active">
-              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+              <a className="nav-link borderlight" href="#">Home <span className="sr-only">(current)</span></a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Link</a>
+              <a className="nav-link borderlight" href="#">About Me</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link disabled" href="#">Disabled</a>
+              <a className="nav-link borderlight" href="#">Projects</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link borderlight" href="#">Contact Me</a>
             </li>
           </ul>
+        </div>
         </div>
       </nav>
     )
