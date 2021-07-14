f28834218816f2975edf76d82b8d9d328e7469f3
hamza887 <hamzabutt887>
2021-07-14 08:33:03 -0400

    working on header

--- myportfolio-app/src/App.js
+++ myportfolio-app/src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
 import Nav from './components/Navbar';
+import Header from './components/Header';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
      <Nav/>
+     <Header/>
 
     </div>
   );
