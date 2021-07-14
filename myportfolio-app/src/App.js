a54f43d5287a0fda2495e2855e0c74b7d4e7468e
hamza887 <hamzabutt887>
2021-07-14 07:30:24 -0400

    creating Navbar

--- myportfolio-app/src/App.js
+++ myportfolio-app/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Nav from './components/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
      <Nav/>
+
     </div>
   );
 }
--- myportfolio-app/src/components/Navbar.js
+++ myportfolio-app/src/components/Navbar.js
@@ -2,6 +2,25 @@ import logo from '../Assets/HZ.png'
 
 export default function Nav(){
     return(
-     <h1>ddhelfrj</h1>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a className="navbar-brand" href="#">HZ</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+      
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Link</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled" href="#">Disabled</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
     )
 }
