bc584398541908d3e9579a433920cd33cd9fc219
hamza887 <hamzabutt887>
2021-07-19 11:39:01 -0400

    adding aboutme

--- myportfolio-app/src/components/AboutMe.js
+++ myportfolio-app/src/components/AboutMe.js
@@ -1,17 +1,17 @@
 export default function Aboutme(){
     return(
         <div className='container py-5'>
-          {/* <div className='row'>
+          <div className='row'>
               <div className='col-lg-6 col-xm-12'>
                 <h1>dfldkjflksdjfldksjfldksjfl</h1>
                 <p>fkdjhglfkjgl;fkdjsg;lfkjsglfksdglfkdsgkjds</p>
               </div>
-             <div className='col-lg-6 col-xm-12'> */}
+             <div className='col-lg-6 col-xm-12'>
                 <h1>About me</h1>
                 <p>kjfldkhfsdlksjfldkjsflkdjlfskdjlsfkjdlskfjdlskfjdlaskjfdlkasjflaskjfldkjfldkasjfldkjflkdjasfldkajsfldkjasfld l fdlsafjldskjfl 
                     hjfdlsfkjdlkjfldsa</p> 
-                {/* </div>         */}
-        {/* //   </div> */}
+                </div>         
+           </div>
         </div>
     )
 }
